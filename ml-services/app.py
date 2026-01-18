030a72171783f07249c6c0cac117d80e120e8b4d
sakethpragallapati <pragallapati.saketh@gmail.com>
2026-01-18 19:51:32 +0530

    Added flask-cors

--- ml-services/app.py
+++ ml-services/app.py
@@ -1,7 +1,10 @@
 from flask import Flask,jsonify,request
+from flask_cors import CORS
 from src.pipeline.predict_pipeline import CustomData,Prediction
 
 app = Flask(__name__)
+CORS(app)
+
 @app.route('/predictData',methods=['POST'])
 def prediction():
 
